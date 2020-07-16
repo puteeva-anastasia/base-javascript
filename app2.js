8ccdb2841feb1d968c39f0326c15829357ca44af
Anastasia Puteeva <anastasiaputeeya@gmail.com>
2020-07-16 18:43:18 +0300

    Add howework 1

---
+++ app2.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("Второе задание!");
+let admin = null;
+let name = "Василий";
+admin = name;
+console.log(admin);
---
+++ app3.js
@@ -0,0 +1,33 @@
+"use strict";
+
+console.log("Третье задание!");
+let result = 10 + 10 + "10";
+console.log(result);
+/*
+1. 10 + 10 = 20
+2. 20 + строка = "2010"
+*/
+result = 10 + "10" + 10;
+console.log(result);
+/*
+1. 10 + строка = 1010
+2. "1010" + 10 = 101010
+*/
+result = 10 + 10 + +"10";
+console.log(result);
+/*
+1. 10 + 10 = 20
+2. Перевод из строки в число: +"10"
+3. 20 + 10 = 30
+*/
+result = 10 / -""; 
+console.log(result);
+/*
+-Infinity
+*/
+result = 10 / +"2.5";
+console.log(result);
+/*
+1. Перевод из строки в число: 2.5
+2. 10 / 2.5 = 4
+*/
